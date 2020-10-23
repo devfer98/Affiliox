d8b0f86b5efc3a62d3aaf50e064e52e09edb733a
Samith Sankalpa <sankalpa.gss@gmail.com>
2020-10-23 19:00:44 +0530

    commit 9

--- promoter/generate-link.php
+++ promoter/generate-link.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Market Place</title>
+	<title>Generate Link</title>
 	<link rel="stylesheet" type="text/css" href="css/generate-link-css.css">
   <link rel="shortcut icon" href="images/affiliox.png" type="image/x-icon">
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
