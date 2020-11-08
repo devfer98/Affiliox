b5e8cc64498fffdf7a5a74f14314ab57abc7a7d0
devfer98 <48157724+devfer98@users.noreply.github.com>
2020-11-08 12:52:48 +0530

    set models 1

--- Core/Connect.php
+++ Core/Connect.php
@@ -3,42 +3,42 @@ namespace Core;
 use PDO;
 
 abstract class Connect{
-    // protected static function connectDB(){
-    //     static $db =null;
-    //     if($db === null){
-    //         $host= "localhost";
-    //         $dbName="test2";
-    //         $user="root";
-    //         $password="";
-
-    //         try {
-    //             $db= new PDO ("mysql:host =$host;dbname=$dbname",$username,$password);
-    //             return $db;
-    //         } catch (PDOException $e) {
-    //             echo $e->getMessage();
-    //         }
-
-    //     }
-
-    // }
-
     protected static function connectDB(){
         static $db =null;
         if($db === null){
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $dbname = "affiliox";
+            $host= "localhost";
+            $dbName="test2";
+            $user="root";
+            $password="";
 
             try {
-                $db = mysqli_connect($servername, $username, $password, $dbname) or die("DB connection failed");
+                $db= new PDO ("mysql:host =$host;dbname=$dbname",$username,$password);
                 return $db;
             } catch (PDOException $e) {
                 echo $e->getMessage();
             }
+
         }
 
     }
+
+    // protected static function connectDB(){
+    //     static $db =null;
+    //     if($db === null){
+    //         $servername = "localhost";
+    //         $username = "root";
+    //         $password = "";
+    //         $dbname = "affiliox";
+
+    //         try {
+    //             $db = mysqli_connect($servername, $username, $password, $dbname) or die("DB connection failed");
+    //             return $db;
+    //         } catch (PDOException $e) {
+    //             echo $e->getMessage();
+    //         }
+    //     }
+
+    // }
 
 }
 
