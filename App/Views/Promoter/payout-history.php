4a4bc4360f6ad772fd5ab803ffeb3926a7ad8d82
Samith Sankalpa <sankalpa.gss@gmail.com>
2020-11-13 20:06:02 +0530

    set promoter routing path

--- App/Views/Promoter/payout-history.php
+++ App/Views/Promoter/payout-history.php
@@ -2,12 +2,12 @@
 <html>
 <head>
 	<title>Payout History</title>
-	<link rel="shortcut icon" href="../../../public/images/promoter/logoOnly.png" type="image/x-icon">
-  <link rel="stylesheet" type="text/css" href="../../../public/css/promoter/payout-history-css.css">
+	<link rel="shortcut icon" href="/images/promoter/logoOnly.png" type="image/x-icon">
+  <link rel="stylesheet" type="text/css" href="/css/promoter/payout-history-css.css">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous"/>
   <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Open+Sans&display=swap" rel="stylesheet">
-  <script src="../../../public/js/promoter/promoter-dashboard-js.js"></script>
+  <script src="/js/promoter/payout-history-js.js"></script>
 </head>
 <body>
 	<!-------------- Top-Navigation-Bar ---------------------->
@@ -15,7 +15,7 @@
       <div class="open-btn" onclick="openNav()">&#9776;</div>
       <nav class="top-nav">
       <ul class="main-nav">
-        <li class="logo"><a href="index.php" style="border: none;"><img src="../../../public/images/promoter/SideLogo.png"></a></li>
+        <li class="logo"><a href="index.php" style="border: none;"><img src="/images/promoter/SideLogo.png"></a></li>
         <li class="item"><a href=""><i class="fas fa-home"></i>&nbsp;Home</a></li>
         <li class="item"><a href=""><i class="fas fa-users"></i>&nbsp;About Us</a></li>
         <li class="item"><a href=""><i class="fas fa-question-circle"></i>&nbsp;Help & Support</a></li>
@@ -55,24 +55,56 @@
 
 			</table>
 		</div> <!-- table -->	
+		<hr style="height:2px;border:none;color:#333;background-color:#333;">
 	</div> <!-- container -->	
 
+		<!-- bottom-part-------------------------------------------->
+		<div class="bottom-part">
+			<table>
+				<tr class="thead">
+					<td rowspan="4"><img src="/images/promoter/affiliox.png" height='150' width='150'></td>
+					<td>Main Menu</td>
+					<td>Find us On</td>
+					<td>Contact us</td>
+				</tr>
+				<tr>
+					<!-- <td></td> -->
+					<td><a href="">About Affiliox <i class="fas fa-users"></i></a></td>
+					<td><a href=""></i>FaceBook <i class="fab fa-facebook-f"></i></a></td>
+					<td>Address: Sri Lanka.</td>
+				</tr>
+				<tr>
+					<!-- <td></td> -->
+					<td><a href="">Help & Support <i class="fas fa-question-circle"></a></td>
+					<td><a href="">Twitter <i class="fab fa-twitter"></i></a></td>
+					<td>Phone: 011-1234567</td>
+				</tr>
+				<tr>
+					<!-- <td></td> -->
+					<td><a href="">Privacy & Policy <i class="fas fa-shield-alt"></i></a></td>
+					<td><a href="">Youtube <i class="fab fa-youtube"></i></a></td>
+					<td>Email: affiliox@gmail.com</td>	
+				</tr>
+			</table>
+		</div> <!-- bottom-part -->
+		
 		<div class="footer">
   			<ul>
   				<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
   				tempor incididunt ut labore et dolore magna aliqua.</li>
-  				<li><a href="">Privacy & Policy</a></li>
-  				<li>© Affiliox</li>
+  				<li> Affiliox &trade;</li>
   			</ul>
-		</div>	<!-- footer -->	
+		</div>	 <!-- footer -->	
+	</div> <!-- navigation -->
+		
 	<div id="mySidenav" class="sidenav">
   		<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-  		<a href="promoter-dashboard.php">User profile<i class="far fa-user"></i></a>
-  		<a href="market-place.php">Marker Place<i class="fab fa-shopify"></i></a>
-  		<a href="promoter-statistics.php">Statstics<i class="fas fa-chart-line"></i></i></a>
-  		<a href="withdraw-earnings.php">Transactions<i class="fas fa-money-check-alt"></i></a>
-  		<a href="review-feedback.php">Feedback<i class="fas fa-phone-square"></i></i></a>
-  		<a href="support-center.php">Support<i class="fas fa-envelope-open-text"></i></a>
+  		<a href="../Promoter/promoterProfile">User profile<i class="far fa-user"></i></a>
+  		<a href="../Promoter/Market">Marker Place<i class="fab fa-shopify"></i></a>
+  		<a href="../Promoter/staticPromoter">Statstics<i class="fas fa-chart-line"></i></i></a>
+  		<a href="../Promoter/promoterTrans">Transactions<i class="fas fa-money-check-alt"></i></a>
+  		<a href="../Promoter/promoterFeedback">Feedback<i class="fas fa-phone-square"></i></i></a>
+  		<a href="../Promoter/promoterSupport">Support<i class="fas fa-envelope-open-text"></i></a>
 	</div>
 	<span onclick="openNav()"></span>
 
--- App/Views/Promoter/withdraw-earnings.php
+++ App/Views/Promoter/withdraw-earnings.php
@@ -2,12 +2,12 @@
 <html>
 <head>
 	<title>Withdraw Earnings</title>
-	<link rel="stylesheet" type="text/css" href="../../../public/css/promoter/withdraw-earnings-css.css">
-	<link rel="shortcut icon" href="../../../public/images/promoter/logoOnly.png" type="image/x-icon">
+	<link rel="stylesheet" type="text/css" href="/css/promoter/withdraw-earnings-css.css">
+	<link rel="shortcut icon" href="/images/promoter/logoOnly.png" type="image/x-icon">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous"/>
 	<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Open+Sans&display=swap" rel="stylesheet">
-	<script src="../../../public/js/promoter/promoter-dashboard-js.js"></script>
+	<script src="/js/promoter/withdraw-earnings-js.js"></script>
 </head>
 <body>
 	<!-------------- Top-Navigation-Bar ---------------------->
@@ -15,7 +15,7 @@
 			<div class="open-btn" onclick="openNav()">&#9776;</div>
 			<nav class="top-nav">
 			<ul class="main-nav">
-				<li class="logo"><a href="index.php" style="border: none;"><img src="../../../public/images/promoter/SideLogo.png"></a></li>
+				<li class="logo"><a href="index.php" style="border: none;"><img src="/images/promoter/SideLogo.png"></a></li>
 				<li class="item"><a href=""><i class="fas fa-home"></i>&nbsp;Home</a></li>
 				<li class="item"><a href=""><i class="fas fa-users"></i>&nbsp;About Us</a></li>
 				<li class="item"><a href=""><i class="fas fa-question-circle"></i>&nbsp;Help & Support</a></li>
@@ -37,10 +37,11 @@
 	<div class="container">
 		<h2><i class="fas fa-money-check-alt"></i>&nbsp; &nbsp;Balance Withdrawal</h2>
 
-		<p><b>Withdraw Earn:</b> Use this page to transfer your balance to your preferred method of payout. You should have minimum payout amount available in your account. To see the list and status of your payouts. <a id="payout-history" href="payout-history.php">CLICK HERE</a></p>
+		<p><b>Withdraw Earn:</b> Use this page to transfer your balance to your preferred method of payout. You should have minimum payout amount available in your account. To see the list and status of your payouts. 
+		<a id="payout-history" href="../Promoter/transHistory">CLICK HERE</a></p>
 
 		<div class="money-withdrawal-form">
-			<form action="withdraw-earnings.php" method="post">
+			<form method="post" action="../Transaction/promoterTransToDB">
 				<table>
 					<tr>
 						<td>Current Amount :</td>
@@ -62,26 +63,56 @@
 			</table>
 			</form>
 		</div> <!-- money-withdrawal-form -->
+		<hr style="height:2px;border:none;color:#333;background-color:#333;">
 	</div> <!-- container -->
 
+		<!-- bottom-part-------------------------------------------->
+		<div class="bottom-part">
+			<table>
+				<tr class="thead">
+					<td rowspan="4"><img src="/images/promoter/affiliox.png" height='150' width='150'></td>
+					<td>Main Menu</td>
+					<td>Find us On</td>
+					<td>Contact us</td>
+				</tr>
+				<tr>
+					<!-- <td></td> -->
+					<td><a href="">About Affiliox <i class="fas fa-users"></i></a></td>
+					<td><a href=""></i>FaceBook <i class="fab fa-facebook-f"></i></a></td>
+					<td>Address: Sri Lanka.</td>
+				</tr>
+				<tr>
+					<!-- <td></td> -->
+					<td><a href="">Help & Support <i class="fas fa-question-circle"></a></td>
+					<td><a href="">Twitter <i class="fab fa-twitter"></i></a></td>
+					<td>Phone: 011-1234567</td>
+				</tr>
+				<tr>
+					<!-- <td></td> -->
+					<td><a href="">Privacy & Policy <i class="fas fa-shield-alt"></i></a></td>
+					<td><a href="">Youtube <i class="fab fa-youtube"></i></a></td>
+					<td>Email: affiliox@gmail.com</td>	
+				</tr>
+			</table>
+		</div> <!-- bottom-part -->
+		
 		<div class="footer">
   			<ul>
   				<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
   				tempor incididunt ut labore et dolore magna aliqua.</li>
-  				<li><a href="">Privacy & Policy</a></li>
-  				<li>© Affiliox</li>
+  				<li> Affiliox &trade;</li>
   			</ul>
-		</div>	<!-- footer -->	
+		</div>	 <!-- footer -->	
 	</div> <!-- navigation -->
 
 	<div id="mySidenav" class="sidenav">
   		<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-  		<a href="promoter-dashboard.php">User profile<i class="far fa-user"></i></a>
-  		<a href="market-place.php">Marker Place<i class="fab fa-shopify"></i></a>
-  		<a href="promoter-statistics.php">Statstics<i class="fas fa-chart-line"></i></i></a>
-  		<a href="withdraw-earnings.php">Transactions<i class="fas fa-money-check-alt"></i></a>
-  		<a href="review-feedback.php">Feedback<i class="fas fa-phone-square"></i></i></a>
-  		<a href="support-center.php">Support<i class="fas fa-envelope-open-text"></i></a>
+  		<a href="../Promoter/promoterProfile">User profile<i class="far fa-user"></i></a>
+  		<a href="../Promoter/Market">Marker Place<i class="fab fa-shopify"></i></a>
+  		<a href="../Promoter/staticPromoter">Statstics<i class="fas fa-chart-line"></i></i></a>
+  		<a href="../Promoter/promoterTrans">Transactions<i class="fas fa-money-check-alt"></i></a>
+  		<a href="../Promoter/promoterFeedback">Feedback<i class="fas fa-phone-square"></i></i></a>
+  		<a href="../Promoter/promoterSupport">Support<i class="fas fa-envelope-open-text"></i></a>
 	</div> <!-- sidenav -->
 	<span onclick="openNav()"></span>
 
