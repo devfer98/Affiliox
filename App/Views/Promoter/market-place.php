8defc0a8a5e83d8859768770de3c69c7db7d1ff4
Samith Sankalpa <sankalpa.gss@gmail.com>
2021-01-16 11:53:03 +0530

    change product card

--- App/Views/Promoter/market-place.php
+++ App/Views/Promoter/market-place.php
@@ -18,96 +18,29 @@
 			<a href="../Promoter/Market"><button type="search" name="submit">Search</button></a>
 		</div> <!-- top-part -->
 
-			<div class="card">
-				<p class = "title">Title:- DJI Mavic Air 2 Fly More Combo</p>
-  				<img src="/images/promoter/drone.jpg" alt="Drone">
-  			<div class="promote-button">
-  				<form action="../Promoter/viewProduct" name="" method="">
-            		<p>Price 99.9 USD</p>
-  					<input id="promoter-button" type="submit" name="" value="View More"></a>
-  				</form>
-  			</div> <!-- promote-button -->
-    			  <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    			  tempor incididunt ut labore et dolore magna aliqua. lorem</h4>
-			</div> <!-- card -->
+			
 
-			<div class="card">
-				<p class = "title">Title:- GoPro Hero 9 New Release</p>
-  				<img src="/images/promoter/action-camera.jpg" alt="Drone">
-  			<div class="promote-button">
-  				<form action="../Promoter/generateLink" name="" method="">
-              		<p>Price 52.9 USD</p>
-  					<input id="promoter-button" type="submit" name="" value="View More"></a>
-  				</form>
-  			</div> <!-- promote-button -->
-    			  <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    			  tempor incididunt ut labore et dolore magna aliqua.</h4>
-			</div> <!-- card -->
+			<div class="product-card">
+				<table>
+					<tr>
+						<td colspan=3 id=title>DJI Mavic Air 2 Fly More Combo</td>
+					</tr>
+					<tr>
+						<td rowspan = 3><img src="/images/promoter/drone.jpg" alt="Drone"></td>
+					</tr>
+					<tr>
+						<td>Product Price: 83.45$</td>
+					</tr>
+					<tr>
+						<td><a href="../Promoter/viewProduct">View More <i class="fas fa-angle-double-right"></i></a></td>
+					</tr>
+					<tr>
+						<td colspan=3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    			  tempor incididunt ut labore et dolore magna aliqua.</td>
+					</tr>
+				</table>
+			</div>
 
-			<div class="card">
-				<p class = "title">Title:- Apple 7+ 128GB ROM - 3GB RAM - Black</p>
-  				<img src="/images/promoter/i-phone.png" alt="Drone">
-  			<div class="promote-button">
-  				<form action="../Promoter/generateLink" name="" method="">
-            		<p>Price 65.9 USD</p>
-  					<input id="promoter-button" type="submit" name="" value="View More"></a>
-  				</form>
-  			</div> <!-- promote-button -->
-    			  <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    			  tempor incididunt ut labore et dolore magna aliqua.</h4>
-			</div> <!-- card -->
-
-			<div class="card">
-				<p class = "title">Title:- Casio Enticer Men's Gold Genuine Leather Analog Watch</p>
-  				<img src="/images/promoter/watch.png" alt="Drone">
-  			<div class="promote-button">
-  				<form action="../Promoter/generateLink" name="" method="">
-            		<p>Price 32.9 USD</p>
-  					<input id="promoter-button" type="submit" name="" value="View More"></a>
-  				</form>
-  			</div> <!-- promote-button -->
-    			  <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    			  tempor incididunt ut labore et dolore magna aliqua.</h4>
-			</div> <!-- card -->
-
-			<div class="card">
-				<p class = "title">Title:- Nike Casual Shoe</p>
-  				<img src="/images/promoter/nike-shoe.png" alt="Drone">
-  			<div class="promote-button">
-  				  <form action="../Promoter/generateLink" name="" method="">
-              		<p>Price 47.9 USD</p>
-  					 <input  id="promoter-button" type="submit" name="" value="View More"></a>
-  				  </form>
-  			</div> <!-- promote-button -->
-    			  <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    			  tempor incididunt ut labore et dolore magna aliqua.</h4>
-			 </div> <!-- card -->
-
-			<div class="card">
-				<p class = "title">Title:- LUMIX 4K Digital Camera DMC-FZ2500GA</p>
-  				<img src="/images/promoter/dslr.jpg" alt="Drone">
-  			<div class="promote-button">
-  				  <form action="../Promoter/generateLink" name="" method="">
-              		<p>Price 47.9 USD</p>
-  					 <input  id="promoter-button" type="submit" name="" value="View More"></a>
-  				  </form>
-  			</div> <!-- promote-button -->
-    			  <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    			  tempor incididunt ut labore et dolore magna aliqua.</h4>
-			 </div> <!-- card -->
-			 
-			 <div class="card">
-				<p class = "title">Title:- i12 Pods TWS Bluetooth 5.0 Pop-up Window Touch Control</p>
-  				<img src="/images/promoter/i pod.png" alt="Drone">
-  			<div class="promote-button">
-  				  <form action="../Promoter/generateLink" name="" method="">
-              		<p>Price 47.9 USD</p>
-  					 <input  id="promoter-button" type="submit" name="" value="View More"></a>
-  				  </form>
-  			</div> <!-- promote-button -->
-    			  <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    			  tempor incididunt ut labore et dolore magna aliqua.</h4>
-			 </div> <!-- card -->
 			 </div> <!-- container -->
 			 
 			
