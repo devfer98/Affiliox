243d87470ca784970c8846817e2a5287100e6a7d
Madhu-26 <madhushadavidmathivannan@gmail.com>
2021-04-01 00:27:50 +0530

    Admin Update Changes

--- App/Models/AdminM.php
+++ App/Models/AdminM.php
@@ -78,42 +78,58 @@ class AdminM extends \Core\Connect{
         }
     }
 
-    public function updateAdmin($userID, $name, $email, $phoneNo, $country, $city, $line1, $line2, $position) {
-        
+    public function updateAdmin($ID, $name, $email, $phoneNo, $country,$gender,$status, $city, $aLine1, $aLine2, $position) {
+        $conn=static::connectDB();
+        $stmt = $conn->prepare("UPDATE Admin SET name= ? , aLine1 = ?, aLine2= ?,city= ?, country= ?, gender= ?,status = ? ,email= ?,phoneNo=  ?,position= ?  WHERE userId=?");
+        $stmt->bind_param("sssssssssss",$name,$aLine1, $aLine2,$city,$country,$gender, $status,$email,$phoneNo,$position,$ID);
+        if ($stmt->execute()) {
+            $stmt->close();
+            return true;
+        }else{
+            echo 'SQL Error';
+            return false;
+        }
     }
-    
-    // public function updateAdmin($data)
-    // {
-       
-    //     $id = $data['userID'];
-    //     $name = $data['name'];
-    //     $status = $data['status'];
-    //     $email = $data['email'];
-    //     $phoneNo = $data['phoneNo'];
-    //     $age = $data['age'];
-    //     $dob = $data['dob'];
-    //     $country = $data['country'];
-    //     $city = $data['city'];
-    //     $aLine1 = $data['aLine1'];
-    //     $aLine2 = $data['aLine2'];
-    //     $position = $data['position'];
-       
-    //     $conn=static::connectDB();
-    //     $query = "UPDATE admin SET name='$name',status='$status',email='$email',phoneNo='$phoneNo', age='$age',dob='$dob', gender='$gender', 
-    //     country='$country', city='$city', aLine1='$line1', aLine2='$line2', position='$position', WHERE userID='$userID'";
-    //     $stmt = $conn->prepare($query);
-    //     $stmt->bind_param("ssssissssssss", $name, $status, $email, $phoneNo, $age, $dob, $gender, $country, $city, $line1, $line2, $position, $userID);
-    //     if ($stmt->execute()) {
-    //         $stmt->close();
-    //         return true;
-    //     }else{
-    //         echo 'SQL Error';
-    //         return false;
-    //     }
-    // }
     
     public function getStatistics() {
 
+    }
+
+    function EmailCompair4admin($email,$ID)
+    {   $conn=static::connectDB();
+        $email2="";
+        $stmt0 = $conn->prepare('select * from admin where userID = ? ');
+        $stmt0->bind_param("s", $ID);
+        if ($stmt0->execute()) {
+            $result = $stmt0->get_result();
+            if ($result->num_rows > 0) {
+                
+                while ($row =  $result->fetch_assoc()) {
+                    
+                    $email2 = $row['email'];
+                }
+            }
+               
+        }
+
+
+        $stmt1 = $conn->prepare('select * from admin where email = ?');
+        $stmt1->bind_param("s", $email);
+        if ($stmt1->execute()) {
+            $result2 = $stmt1->get_result();
+            if ($result2 ->num_rows > 0) {
+                
+                while ($row =  $result2->fetch_assoc()) {
+                   
+                   if( $email2 == $row['email']){
+                    return true;
+                   }else{
+                    return false;
+                   }
+                }
+            }
+               return true;
+        }
     }
 
     function EmailCompair($email,$username){
